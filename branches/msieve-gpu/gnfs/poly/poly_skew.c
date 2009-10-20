c1591ce11f40e6b12b043bc12c84d81cc885d223
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2009-10-20 13:10:54 +0000

    add a few degree 6 parameters

--- branches/msieve-gpu/gnfs/poly/poly_skew.c
+++ branches/msieve-gpu/gnfs/poly/poly_skew.c
@@ -108,9 +108,9 @@ static const poly_param_t prebuilt_params_deg5[] = {
 	{165, 8.00E+024, 2.00E+023, 2.50E-013},
 	{170, 5.00E+025, 1.58E+024, 1.20E-013},
 
-	/* irresponsibly interpolated by Serge Batalov */
+	/* contributed by Serge Batalov */
 
-	{175, 3.00E+026, 1.00E+025, 6.00E-014}, /* not 1.00E-013 ! */
+	{175, 3.00E+026, 1.00E+025, 6.00E-014},
 	{180, 1.80E+027, 5.36E+025, 2.50E-014},
 	{185, 1.00E+028, 3.12E+026, 1.00E-014},
 	{190, 6.00E+028, 1.82E+027, 4.00E-015},
@@ -118,10 +118,13 @@ static const poly_param_t prebuilt_params_deg5[] = {
 
 static const poly_param_t prebuilt_params_deg6[] = {
 
-	/* complete guesses */
-
 	{140, 2.34E+017, 5.00E+018, 1.7e-012},
 	{141, 2.34E+017, 5.00E+018, 1.7e-012},
+
+	/* contributed by Paul Leyland */
+
+ 	{200, 1.00E+031, 1.00E+030, 8.0e-018},
+ 	{205, 1.00E+032, 1.00E+031, 6.0e-019},
 };
 
 /*--------------------------------------------------------------------*/
