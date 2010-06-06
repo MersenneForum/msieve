31fe377b3e9fc053f8f0a3a11a935ed3b45a705a
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2010-06-06 00:19:33 +0000

    heapify the number of XY planes that are sieved, to cap the amount of work performed and avoid having to guess a good cutoff

--- branches/deg6/gnfs/poly/stage2/root_sieve_deg6_xy.c
+++ branches/deg6/gnfs/poly/stage2/root_sieve_deg6_xy.c
@@ -33,11 +33,86 @@ typedef struct {
 typedef struct {
 	uint32 p;
 	uint32 table_size;
-	uint16 target_sieve_val;
 	uint32 num_powers;
 	xypower_t *powers;
 	uint16 *sieve;
 } xydata_t;
+
+
+
+typedef struct {
+	lattice_t plane;
+	uint32 which_z_block;
+	uint32 which_lattice_xyz;
+} plane_t;
+
+#define PLANE_HEAP_SIZE 20
+
+typedef struct {
+	uint32 num_entries;
+	plane_t entries[PLANE_HEAP_SIZE];
+} plane_heap_t;
+
+/*-------------------------------------------------------------------------*/
+/* boilerplate code for managing heaps */
+
+#define HEAP_SWAP(a,b) { tmp = a; a = b; b = tmp; }
+#define HEAP_PARENT(i)  (((i)-1) >> 1)
+#define HEAP_LEFT(i)    (2 * (i) + 1)
+#define HEAP_RIGHT(i)   (2 * (i) + 2)
+
+static void
+heapify(plane_t *h, uint32 index, uint32 size) {
+
+	uint32 c;
+	plane_t tmp;
+	for (c = HEAP_LEFT(index); c < (size-1); 
+			index = c, c = HEAP_LEFT(index)) {
+
+		if (h[c].plane.score > h[c+1].plane.score)
+			c++;
+
+		if (h[index].plane.score > h[c].plane.score) {
+			HEAP_SWAP(h[index], h[c]);
+		}
+		else {
+			return;
+		}
+	}
+	if (c == (size-1) && h[index].plane.score > h[c].plane.score) {
+		HEAP_SWAP(h[index], h[c]);
+	}
+}
+
+static void
+make_heap(plane_t *h, uint32 size) {
+
+	int32 i;
+	for (i = HEAP_PARENT(size); i >= 0; i--)
+		heapify(h, (uint32)i, size);
+}
+
+static void
+save_plane(plane_heap_t *heap, lattice_t *lattice, 
+		uint32 which_z_block, uint32 which_lattice_xyz)
+{
+	plane_t *h = heap->entries;
+
+	if (heap->num_entries <= PLANE_HEAP_SIZE - 1) {
+		plane_t *s = h + heap->num_entries++;
+		s->plane = *lattice;
+		s->which_z_block = which_z_block;
+		s->which_lattice_xyz = which_lattice_xyz;
+		if (heap->num_entries == PLANE_HEAP_SIZE)
+			make_heap(h, PLANE_HEAP_SIZE);
+	}
+	else if (h->plane.score < lattice->score) {
+		h->plane = *lattice;
+		h->which_z_block = which_z_block;
+		h->which_lattice_xyz = which_lattice_xyz;
+		heapify(h, 0, PLANE_HEAP_SIZE);
+	}
+}
 
 /*-------------------------------------------------------------------------*/
 static uint32
@@ -110,13 +185,13 @@ find_lattice_primes(sieve_prime_t *primes, uint32 num_primes,
 }
 
 /*-------------------------------------------------------------------------*/
-#define MAX_XY_LATTICES 10
+#define MAX_XY_LATTICES 32
 
 static void 
 root_sieve_xy(root_sieve_t *rs, xydata_t *xydata, 
 		uint32 num_lattice_primes, 
-		lattice_t *lattice_xyz, int64 z_base,
-		double line_min, double line_max)
+		uint32 which_z_block, uint32 which_lattice_xyz,
+		plane_heap_t *plane_heap)
 {
 	uint32 i, j, k;
 	hit_t hitlist[MAX_CRT_FACTORS];
@@ -124,23 +199,20 @@ root_sieve_xy(root_sieve_t *rs, xydata_t *xydata,
 	sieve_xy_t *xy = &rs->xydata;
 	uint32 num_lattices = 1;
 
-	if (line_min >= line_max)
-		return;
-
 	for (i = 0; i < num_lattice_primes; i++) {
 
 		xydata_t *curr_xydata = xydata + i;
 		uint32 p = curr_xydata->p;
 		uint32 table_size = curr_xydata->table_size;
 		uint16 *sieve = curr_xydata->sieve;
-		uint32 target_sieve_val = curr_xydata->target_sieve_val;
+		uint32 max_sieve_val = 0;
 		hit_t *hits = hitlist + i;
 
 		for (j = k = 0; j < table_size; j++) {
-			if (sieve[j] >= target_sieve_val) {
+			if (sieve[j] >= max_sieve_val) {
 
-				if (sieve[j] > target_sieve_val) {
-					target_sieve_val = sieve[j];
+				if (sieve[j] > max_sieve_val) {
+					max_sieve_val = sieve[j];
 					k = 0;
 				}
 
@@ -160,57 +232,26 @@ root_sieve_xy(root_sieve_t *rs, xydata_t *xydata,
 	num_lattices = MIN(num_lattices, MAX_XY_LATTICES);
 
 	compute_lattices(hitlist, num_lattice_primes,
-			lattices_xy, xy->lattice_size, num_lattices, 2);
+			lattices_xy, xy->lattice_size, 
+			num_lattices, 2);
 
-	xy->apoly = rs->apoly;
-	xy->apoly.coeff[3] += z_base * rs->dbl_p;
-	xy->apoly.coeff[2] -= z_base * rs->dbl_d;
+	if (plane_heap->num_entries < PLANE_HEAP_SIZE ||
+	    lattices_xy[0].score > plane_heap->entries[0].plane.score) {
 
-	mpz_set_d(xy->tmp1, line_min);
-	mpz_tdiv_q(xy->y_base, xy->tmp1, xy->mp_lattice_size);
-	mpz_mul(xy->y_base, xy->y_base, xy->mp_lattice_size);
-	xy->y_blocks = (line_max - line_min) / xy->dbl_lattice_size;
-
-	for (i = 0; i < num_lattices; i++) {
-
-		lattice_t *lattice_xy = lattices_xy + i;
-
-		uint64_2gmp(lattice_xy->x, xy->tmp1);
-		uint64_2gmp(lattice_xyz->x, xy->tmp2);
-		mpz_mul(xy->resclass_x, xy->tmp1, xy->crt0);
-		mpz_addmul(xy->resclass_x, xy->tmp2, xy->crt1);
-
-		uint64_2gmp(lattice_xy->y, xy->tmp1);
-		uint64_2gmp(lattice_xyz->y, xy->tmp2);
-		mpz_mul(xy->resclass_y, xy->tmp1, xy->crt0);
-		mpz_addmul(xy->resclass_y, xy->tmp2, xy->crt1);
-
-		mpz_tdiv_r(xy->resclass_x, xy->resclass_x, 
-				xy->mp_lattice_size);
-		mpz_tdiv_r(xy->resclass_y, xy->resclass_y, 
-				xy->mp_lattice_size);
-
-		xy->curr_score = lattice_xyz->score + lattice_xy->score;
-		rs->curr_z = z_base + lattice_xyz->z; 
-
-		sieve_x_run(rs);
+		for (i = 0; i < num_lattices; i++) {
+			save_plane(plane_heap, lattices_xy + i,
+					which_z_block, 
+					which_lattice_xyz);
+		}
 	}
 }
 
 /*-------------------------------------------------------------------------*/
-static int compare_xydata(const void *x, const void *y)
-{
-	xydata_t *xx = (xydata_t *)x;
-	xydata_t *yy = (xydata_t *)y;
-	return xx->p - yy->p;
-}
-
 static void
 xydata_alloc(sieve_prime_t *lattice_primes, 
 		uint32 num_lattice_primes, 
 		uint64 lattice_size_xyz,
-		xydata_t *xydata,
-		double num_tests)
+		xydata_t *xydata)
 {
 	uint32 i, j, k;
 
@@ -221,7 +262,6 @@ xydata_alloc(sieve_prime_t *lattice_primes,
 		uint32 num_powers = curr_prime->num_powers;
 		uint32 p = curr_prime->powers[num_powers-1].power;
 		uint32 table_size = p * p;
-		uint32 target_sieve_val = 0;
 
 		curr_xydata->p = p;
 		curr_xydata->table_size = table_size;
@@ -236,7 +276,6 @@ xydata_alloc(sieve_prime_t *lattice_primes,
 			xypower_t *curr_xypower = curr_xydata->powers + j;
 			uint32 power = sp->power;
 			uint32 num_roots = sp->num_roots;
-			uint32 target_roots = 0;
 	
 			curr_xypower->power = power;
 			curr_xypower->latsize_mod = lattice_size_xyz % power;
@@ -244,21 +283,6 @@ xydata_alloc(sieve_prime_t *lattice_primes,
 			curr_xypower->contrib = sp->sieve_contrib;
 			curr_xypower->roots = (xyprog_t *)xmalloc(num_roots * 
 							sizeof(xyprog_t));
-
-			if (j == 0) {
-				if (num_tests < 100000)
-					target_roots = 3;
-				else
-					target_roots = 4;
-			}
-			else {
-				if (curr_prime->prime == 2)
-					target_roots = num_roots - 1;
-				else
-					target_roots = num_roots / 2;
-			}
-			target_sieve_val += curr_xypower->contrib *
-					MIN(num_roots, target_roots);
 
 			for (k = 0; k < num_roots; k++) {
 				sieve_root_t *r = sp->roots + k;
@@ -270,11 +294,7 @@ xydata_alloc(sieve_prime_t *lattice_primes,
 					(uint8 *)xmalloc(power * sizeof(uint8));
 			}
 		}
-
-		curr_xydata->target_sieve_val = target_sieve_val;
 	}
-
-	qsort(xydata, num_lattice_primes, sizeof(xydata_t), compare_xydata);
 }
 
 /*-------------------------------------------------------------------------*/
@@ -383,6 +403,9 @@ do_sieving(xydata_t *curr_xydata)
 			row[curr_start] += contrib;
 			row += p;
 		}
+
+		curr_prog->start = mp_modsub_1(start, 
+					curr_prog->stride_z, p);
 	}
 }
 
@@ -425,6 +448,9 @@ do_sieving_powers(xydata_t *curr_xydata)
 					row += p;
 				}
 			}
+
+			curr_prog->start = mp_modsub_1(start, 
+						curr_prog->stride_z, p);
 		}
 	}
 }
@@ -433,12 +459,11 @@ do_sieving_powers(xydata_t *curr_xydata)
 static void 
 find_hits(root_sieve_t *rs, xydata_t *xydata, 
 		uint32 num_lattice_primes, 
-		lattice_t *lattice_xyz)
+		uint32 which_lattice_xyz,
+		plane_heap_t *plane_heap)
 {
-	uint32 i, j, k, m;
+	uint32 i, j;
 	sieve_xyz_t *xyz = &rs->xyzdata;
-	uint64 lattice_size_xyz = xyz->lattice_size;
-	int64 z_base = xyz->z_base;
 	uint32 z_blocks = xyz->z_blocks;
 
 	for (i = 0; i < z_blocks; i++) {
@@ -447,7 +472,6 @@ find_hits(root_sieve_t *rs, xydata_t *xydata,
 
 			xydata_t *curr_xydata = xydata + j;
 			uint32 table_size = curr_xydata->table_size;
-			uint32 target_sieve_val = curr_xydata->target_sieve_val;
 			uint16 *sieve = curr_xydata->sieve;
 
 			memset(sieve, 0, table_size * sizeof(uint16));
@@ -456,44 +480,13 @@ find_hits(root_sieve_t *rs, xydata_t *xydata,
 				do_sieving(curr_xydata);
 			else
 				do_sieving_powers(curr_xydata);
-
-
-			for (k = 0; k < table_size; k++) {
-				if (sieve[k] >= target_sieve_val)
-					break;
-			}
-			if (k == table_size)
-				break;
 		}
 
-		if (j == num_lattice_primes) {
+		if (xyz->y_line_min[i] < xyz->y_line_max[i]) {
 			root_sieve_xy(rs, xydata, 
 					num_lattice_primes,
-					lattice_xyz, 
-					z_base + i * lattice_size_xyz,
-					xyz->y_line_min[i],
-					xyz->y_line_max[i]);
-		}
-
-		for (j = 0; j < num_lattice_primes; j++) {
-
-			xydata_t *curr_xydata = xydata + j;
-			uint32 num_powers = curr_xydata->num_powers;
-
-			for (k = 0; k < num_powers; k++) {
-				xypower_t *curr_xypower = curr_xydata->powers+k;
-				uint32 p = curr_xypower->power;
-				uint32 num_roots = curr_xypower->num_roots;
-				xyprog_t *roots = curr_xypower->roots;
-
-				for (m = 0; m < num_roots; m++) {
-					xyprog_t *curr_prog = roots + m;
-					curr_prog->start = mp_modsub_1(
-							curr_prog->start,
-							curr_prog->stride_z, 
-							p);
-				}
-			}
+					i, which_lattice_xyz,
+					plane_heap);
 		}
 	}
 }
@@ -536,7 +529,6 @@ sieve_xy_run(root_sieve_t *rs)
 {
 	uint32 i;
 
-	poly_stage2_t *data = (poly_stage2_t *)rs->root_heap.extra;
 	sieve_xyz_t *xyz = &rs->xyzdata;
 	int64 z_base = xyz->z_base;
 
@@ -547,6 +539,7 @@ sieve_xy_run(root_sieve_t *rs)
 	double direction[3] = {0, 1, 0};
 	double line_min, line_max;
 	xydata_t xydata[MAX_CRT_FACTORS];
+	plane_heap_t plane_heap;
 
 	uint64 inv_xy;
 	uint64 inv_xyz;
@@ -575,28 +568,64 @@ sieve_xy_run(root_sieve_t *rs)
 	xy->dbl_lattice_size = mpz_get_d(xy->mp_lattice_size);
 
 	xydata_alloc(lattice_primes, num_lattice_primes, 
-			xyz->lattice_size, xydata, 
-			(double)xyz->z_blocks * xyz->num_lattices);
+			xyz->lattice_size, xydata);
+
+	plane_heap.num_entries = 0;
 
 	for (i = 0; i < xyz->num_lattices; i++) {
 
 		lattice_t *curr_lattice_xyz = xyz->lattices + i;
 
-#if 0
-		if (i % 10 == 0) {
-			printf(".");
-			fflush(stdout);
-		}
-#endif
 		xydata_init(xydata, num_lattice_primes,
 				curr_lattice_xyz, z_base);
 
 		find_hits(rs, xydata, num_lattice_primes, 
-				curr_lattice_xyz);
-
-		if (data->obj->flags & MSIEVE_FLAG_STOP_SIEVING)
-			break;
+				i, &plane_heap);
 	}
 
 	xydata_free(xydata, num_lattice_primes);
+
+	for (i = 0; i < plane_heap.num_entries; i++) {
+
+		plane_t *curr_plane = plane_heap.entries + i;
+		lattice_t *lattice_xy = &curr_plane->plane;
+		lattice_t *lattice_xyz = xyz->lattices + 
+					curr_plane->which_lattice_xyz;
+
+		line_min = xyz->y_line_min[curr_plane->which_z_block];
+		line_max = xyz->y_line_max[curr_plane->which_z_block];
+		z_base = xyz->z_base + curr_plane->which_z_block *
+					xyz->lattice_size;
+
+		xy->apoly = rs->apoly;
+		xy->apoly.coeff[3] += z_base * rs->dbl_p;
+		xy->apoly.coeff[2] -= z_base * rs->dbl_d;
+
+		mpz_set_d(xy->tmp1, line_min);
+		mpz_tdiv_q(xy->y_base, xy->tmp1, xy->mp_lattice_size);
+		mpz_mul(xy->y_base, xy->y_base, xy->mp_lattice_size);
+		xy->y_blocks = (line_max - line_min) / xy->dbl_lattice_size;
+
+		uint64_2gmp(lattice_xy->x, xy->tmp1);
+		uint64_2gmp(lattice_xyz->x, xy->tmp2);
+		mpz_mul(xy->resclass_x, xy->tmp1, xy->crt0);
+		mpz_addmul(xy->resclass_x, xy->tmp2, xy->crt1);
+
+		uint64_2gmp(lattice_xy->y, xy->tmp1);
+		uint64_2gmp(lattice_xyz->y, xy->tmp2);
+		mpz_mul(xy->resclass_y, xy->tmp1, xy->crt0);
+		mpz_addmul(xy->resclass_y, xy->tmp2, xy->crt1);
+
+		mpz_tdiv_r(xy->resclass_x, xy->resclass_x, 
+				xy->mp_lattice_size);
+		mpz_tdiv_r(xy->resclass_y, xy->resclass_y, 
+				xy->mp_lattice_size);
+
+		xy->curr_score = lattice_xyz->score + lattice_xy->score;
+		printf("-------------- %u (%u)\n", xy->curr_score,
+				curr_plane->which_z_block);
+		rs->curr_z = z_base + lattice_xyz->z; 
+
+		sieve_x_run(rs);
+	}
 }
