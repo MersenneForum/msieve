ebfc839c5f672aa38dc1f21c7d7dfa99efb91f2a
jaysonking <jaysonking@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2011-09-19 06:30:35 +0000

    branch for additional polynomial selection experiments.

---
+++ branches/cuda11_test/demo.c
@@ -0,0 +1,600 @@
+/*--------------------------------------------------------------------
+This source distribution is placed in the public domain by its author,
+Jason Papadopoulos. You may use it for any purpose, free of charge,
+without having to notify anyone. I disclaim any responsibility for any
+errors.
+
+Optionally, please be nice and tell me if you find this source to be
+useful. Again optionally, if you add to the functionality present here
+please consider making those additions public too, so that others may 
+benefit from your work.	
+
+$Id$
+--------------------------------------------------------------------*/
+
+#include <msieve.h>
+#include <signal.h>
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+msieve_obj *g_curr_factorization = NULL;
+
+/*--------------------------------------------------------------------*/
+void handle_signal(int sig) {
+
+	msieve_obj *obj = g_curr_factorization;
+
+	printf("\nreceived signal %d; shutting down\n", sig);
+	
+	if (obj && (obj->flags & MSIEVE_FLAG_SIEVING_IN_PROGRESS))
+		obj->flags |= MSIEVE_FLAG_STOP_SIEVING;
+	else
+		_exit(0);
+}
+
+/*--------------------------------------------------------------------*/
+void get_random_seeds(uint32 *seed1, uint32 *seed2) {
+
+	uint32 tmp_seed1, tmp_seed2;
+
+	/* In a multithreaded program, every msieve object
+	   should have two unique, non-correlated seeds
+	   chosen for it */
+
+#if !defined(WIN32) && !defined(_WIN64)
+
+	FILE *rand_device = fopen("/dev/urandom", "r");
+
+	if (rand_device != NULL) {
+
+		/* Yay! Cryptographic-quality nondeterministic randomness! */
+
+		fread(&tmp_seed1, sizeof(uint32), (size_t)1, rand_device);
+		fread(&tmp_seed2, sizeof(uint32), (size_t)1, rand_device);
+		fclose(rand_device);
+	}
+	else
+
+#endif
+	{
+		/* <Shrug> For everyone else, sample the current time,
+		   the high-res timer (hopefully not correlated to the
+		   current time), and the process ID. Multithreaded
+		   applications should fold in the thread ID too */
+
+		uint64 high_res_time = read_clock();
+		tmp_seed1 = ((uint32)(high_res_time >> 32) ^
+			     (uint32)time(NULL)) * 
+			    (uint32)getpid();
+		tmp_seed2 = (uint32)high_res_time;
+	}
+
+	/* The final seeds are the result of a multiplicative
+	   hash of the initial seeds */
+
+	(*seed1) = tmp_seed1 * ((uint32)40499 * 65543);
+	(*seed2) = tmp_seed2 * ((uint32)40499 * 65543);
+}
+
+/*--------------------------------------------------------------------*/
+void print_usage(char *progname) {
+
+	printf("\nMsieve v. %d.%02d (SVN %s)\n", MSIEVE_MAJOR_VERSION, 
+					MSIEVE_MINOR_VERSION,
+					MSIEVE_SVN_VERSION);
+
+	printf("\nusage: %s [options] [one_number]\n", progname);
+	printf("\nnumbers starting with '0' are treated as octal,\n"
+		"numbers starting with '0x' are treated as hexadecimal\n");
+	printf("\noptions:\n"
+	         "   -s <name> save intermediate results to <name>\n"
+		 "             instead of the default %s\n"
+	         "   -l <name> append log information to <name>\n"
+		 "             instead of the default %s\n"
+	         "   -i <name> read one or more integers to factor from\n"
+		 "             <name> (default worktodo.ini) instead of\n"
+		 "             from the command line\n"
+		 "   -m        manual mode: enter numbers via standard input\n"
+		 "   -mb <num> hint for number of megabytes of memory for\n"
+		 "             postprocessing (set automatically if unspec-\n"
+		 "             ified or zero)\n"
+	         "   -q        quiet: do not generate any log information,\n"
+		 "             only print any factors found\n"
+	         "   -d <min>  deadline: if still sieving after <min>\n"
+		 "             minutes, shut down gracefully (default off)\n"
+		 "   -r <num>  stop sieving after finding <num> relations\n"
+		 "   -p        run at idle priority\n"
+	         "   -v        verbose: write log information to screen\n"
+		 "             as well as to logfile\n"
+#ifdef HAVE_CUDA
+		 "   -g <num>  use GPU <num>, 0 <= num < (# graphics cards)>\n"
+#endif
+	         "   -t <num>  use at most <num> threads\n"
+		 "   -D <num>  make filtering target <num> nonzeros per\n"
+		 "             matrix column (default value if 0)\n\n"
+		 " elliptic curve options:\n"
+		 "   -e        perform 'deep' ECM, seek factors > 15 digits\n\n"
+		 " quadratic sieve options:\n"
+		 "   -c        client: only perform sieving\n\n"
+		 " number field sieve options:\n"
+		 "   -n        use the number field sieve (80+ digits only;\n"
+		 "             performs all NFS tasks in order)\n"
+	         "   -nf <name> read from / write to NFS factor base file\n"
+		 "             <name> instead of the default %s\n"
+		 "   -np [X,Y] perform only NFS polynomial selection; if\n"
+		 "             specified, only cover leading coefficients\n"
+		 "             in the range from X to Y inclusive\n"
+		 "   -np1 [X,Y] perform stage 1 of NFS polynomial selection; if\n"
+		 "             specified, only cover leading coefficients\n"
+		 "             in the range from X to Y inclusive\n"
+		 "   -np2      perform stage 2 of NFS polynomial selection\n"
+		 "   -ns [X,Y] perform only NFS sieving; if specified,\n"
+		 "             handle sieve lines X to Y inclusive\n"
+		 "   -nc       perform only NFS combining (all phases)\n"
+		 "   -nc1 [X,Y] perform only NFS filtering. Filtering will\n"
+		 "             track ideals >= X (determined automatically\n"
+		 "             if 0 or unspecified) and will only use the \n"
+		 "             first Y relations (or all relations, if 0 \n"
+		 "             or unspecified)\n"
+#ifdef HAVE_MPI
+		 "   -nc2 [X,Y] perform only NFS linear algebra\n"
+		 "   -ncr [X,Y] perform only NFS linear algebra, restarting\n"
+		 "             from a previous checkpoint\n"
+		 "             The X,Y arguments create a X x Y MPI grid,\n"
+		 "             with default of 1 x (argument to mpirun)\n"
+#else
+		 "   -nc2      perform only NFS linear algebra\n"
+		 "   -ncr      perform only NFS linear algebra, restarting\n"
+		 "             from a previous checkpoint\n"
+#endif
+		 "   -nc3 [X,Y] perform only NFS square root (compute \n"
+		 "             dependency numbers X through Y, 1<=X<=Y<=64)\n",
+		 MSIEVE_DEFAULT_SAVEFILE, MSIEVE_DEFAULT_LOGFILE,
+		 MSIEVE_DEFAULT_NFS_FBFILE);
+}
+
+/*--------------------------------------------------------------------*/
+void factor_integer(char *buf, uint32 flags,
+		    char *savefile_name,
+		    char *logfile_name,
+		    char *nfs_fbfile_name,
+		    uint32 *seed1, uint32 *seed2,
+		    uint32 max_relations,
+		    uint64 nfs_lower,
+		    uint64 nfs_upper,
+		    enum cpu_type cpu,
+		    uint32 cache_size1,
+		    uint32 cache_size2,
+		    uint32 num_threads,
+		    uint32 mem_mb,
+		    uint32 which_gpu,
+		    double target_density) {
+	
+	char *int_start, *last;
+	msieve_obj *obj;
+	msieve_factor *factor;
+
+	/* point to the start of the integer or expression;
+	   if the start point indicates no integer is present,
+	   don't try to factor it :) */
+
+	last = strchr(buf, '\n');
+	if (last)
+		*last = 0;
+	int_start = buf;
+	while (*int_start && !isdigit(*int_start) &&
+			*int_start != '(' ) {
+		int_start++;
+	}
+	if (*int_start == 0)
+		return;
+
+	g_curr_factorization = msieve_obj_new(int_start, flags,
+					savefile_name, logfile_name,
+					nfs_fbfile_name,
+					*seed1, *seed2, max_relations,
+					nfs_lower, nfs_upper, cpu,
+					cache_size1, cache_size2,
+					num_threads, mem_mb, which_gpu,
+					target_density);
+	if (g_curr_factorization == NULL) {
+		printf("factoring initialization failed\n");
+		return;
+	}
+
+	msieve_run(g_curr_factorization);
+
+	if (!(g_curr_factorization->flags & MSIEVE_FLAG_FACTORIZATION_DONE)) {
+		printf("\ncurrent factorization was interrupted\n");
+		exit(0);
+	}
+
+	/* If no logging is specified, at least print out the
+	   factors that were found */
+
+	if (!(g_curr_factorization->flags & (MSIEVE_FLAG_USE_LOGFILE |
+					MSIEVE_FLAG_LOG_TO_STDOUT))) {
+		factor = g_curr_factorization->factors;
+
+		printf("\n");
+		printf("%s\n", buf);
+		while (factor != NULL) {
+			char *factor_type;
+
+			if (factor->factor_type == MSIEVE_PRIME)
+				factor_type = "p";
+			else if (factor->factor_type == MSIEVE_COMPOSITE)
+				factor_type = "c";
+			else
+				factor_type = "prp";
+
+			printf("%s%d: %s\n", factor_type, 
+					(int32)strlen(factor->number), 
+					factor->number);
+			factor = factor->next;
+		}
+		printf("\n");
+	}
+
+	/* save the current value of the random seeds, so that
+	   the next factorization will pick up the pseudorandom
+	   sequence where this factorization left off */
+
+	*seed1 = g_curr_factorization->seed1;
+	*seed2 = g_curr_factorization->seed2;
+
+	/* free the current factorization struct. The following
+	   avoids a race condition in the signal handler */
+
+	obj = g_curr_factorization;
+	g_curr_factorization = NULL;
+	if (obj)
+		msieve_obj_free(obj);
+}
+
+#ifdef WIN32
+DWORD WINAPI countdown_thread(LPVOID pminutes) {
+	DWORD minutes = *(DWORD *)pminutes;
+
+	if (minutes > 0x7fffffff / 60000)
+		minutes = 0;            /* infinite */
+
+	Sleep(minutes * 60000);
+	raise(SIGINT);
+	return 0;
+}
+
+#else
+void *countdown_thread(void *pminutes) {
+	uint32 minutes = *(uint32 *)pminutes;
+
+	if (minutes > 0xffffffff / 60)
+		minutes = 0xffffffff / 60;   /* infinite */
+
+	sleep(minutes * 60);
+	raise(SIGINT);
+	return NULL;
+}
+#endif
+
+/*--------------------------------------------------------------------*/
+int main(int argc, char **argv) {
+
+	char buf[400];
+	uint32 seed1, seed2;
+	char *savefile_name = NULL;
+	char *logfile_name = NULL;
+	char *infile_name = "worktodo.ini";
+	char *nfs_fbfile_name = NULL;
+	uint32 flags;
+	char manual_mode = 0;
+	int i;
+	int32 deadline = 0;
+	uint32 max_relations = 0;
+	uint64 nfs_lower = 0;
+	uint64 nfs_upper = 0;
+	enum cpu_type cpu;
+	uint32 cache_size1; 
+	uint32 cache_size2; 
+	uint32 num_threads = 0;
+	uint32 mem_mb = 0;
+	uint32 which_gpu = 0;
+	double target_density = 0;
+		
+	get_cache_sizes(&cache_size1, &cache_size2);
+	cpu = get_cpu_type();
+
+	if (signal(SIGINT, handle_signal) == SIG_ERR) {
+	        printf("could not install handler on SIGINT\n");
+	        return -1;
+	}
+	if (signal(SIGTERM, handle_signal) == SIG_ERR) {
+	        printf("could not install handler on SIGTERM\n");
+	        return -1;
+	}     
+#ifdef HAVE_MPI
+	{
+		int32 level;
+		if ((i = MPI_Init_thread(&argc, &argv,
+				MPI_THREAD_FUNNELED, &level)) != MPI_SUCCESS) {
+			printf("error %d initializing MPI, aborting\n", i);
+			MPI_Abort(MPI_COMM_WORLD, i);
+		}
+	}
+#endif
+
+	flags = MSIEVE_FLAG_USE_LOGFILE;
+
+	i = 1;
+	buf[0] = 0;
+	while (i < argc) {
+		if (argv[i][0] == (char)('-')) {
+			switch(argv[i][1]) {
+			case 'h':
+			case '?':
+				print_usage(argv[0]);
+				return 0;
+
+			case 'i':
+			case 's':
+			case 'l':
+				if (i + 1 < argc && 
+				    argv[i+1][0] != '-') {
+					if (tolower(argv[i][1]) == 'i')
+						infile_name = argv[i+1];
+					else if (tolower(argv[i][1]) == 's') {
+						char *p;
+						savefile_name = argv[i+1];
+						if((p=strstr(savefile_name, ".gz"))) {
+							savefile_name = strdup(argv[i+1]);
+							savefile_name[p-argv[i+1]] = 0;
+						}
+					} else
+						logfile_name = argv[i+1];
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+					
+			case 'm':
+				if (argv[i][2] == 'b' &&
+				    i + 1 < argc && isdigit(argv[i+1][0])) {
+					mem_mb = atol(argv[i+1]);
+					i += 2;
+				}
+				else {
+					manual_mode = 1;
+					i++;
+				}
+				break;
+
+			case 'e':
+				flags |= MSIEVE_FLAG_DEEP_ECM;
+				i++;
+				break;
+
+			case 'n':
+				if (argv[i][2] == 'p') {
+					if (argv[i][3] == '1')
+						flags |= MSIEVE_FLAG_NFS_POLY1;
+					else if (argv[i][3] == '2')
+						flags |= MSIEVE_FLAG_NFS_POLY2;
+					else
+						flags |= MSIEVE_FLAG_NFS_POLY1 |
+							 MSIEVE_FLAG_NFS_POLY2;
+				}
+				else if (argv[i][2] == 's') {
+					flags |= MSIEVE_FLAG_NFS_SIEVE;
+				}
+				else if (argv[i][2] == 'c') {
+					if (argv[i][3] == '1')
+						flags |= MSIEVE_FLAG_NFS_FILTER;
+					else if (argv[i][3] == '2')
+						flags |= MSIEVE_FLAG_NFS_LA;
+					else if (argv[i][3] == 'r')
+						flags |= 
+						     MSIEVE_FLAG_NFS_LA |
+						     MSIEVE_FLAG_NFS_LA_RESTART;
+					else if (argv[i][3] == '3')
+						flags |= MSIEVE_FLAG_NFS_SQRT;
+					else if (argv[i][3] == 0)
+						flags |= 
+						     MSIEVE_FLAG_NFS_FILTER |
+						     MSIEVE_FLAG_NFS_LA |
+						     MSIEVE_FLAG_NFS_SQRT;
+				}
+				else if (argv[i][2] == 0) {
+					flags |= MSIEVE_FLAG_NFS_POLY1 |
+						 MSIEVE_FLAG_NFS_POLY2 |
+						 MSIEVE_FLAG_NFS_SIEVE |
+						 MSIEVE_FLAG_NFS_FILTER |
+						 MSIEVE_FLAG_NFS_LA |
+						 MSIEVE_FLAG_NFS_SQRT;
+				}
+
+				if (i + 1 < argc && argv[i+1][0] != '-') {
+					if (argv[i][2] == 'f') {
+						nfs_fbfile_name = argv[i+1];
+						i++;
+					}
+					else if ((argv[i][2] == 's' ||
+						  argv[i][2] == 'c' ||
+						  argv[i][2] == 'p') &&
+						  strchr(argv[i+1], ',') != 
+						  		NULL ) {
+						char *tmp;
+						nfs_lower = strtoull(argv[i+1],
+								&tmp, 10);
+						tmp++;
+						nfs_upper = strtoull(tmp,
+								NULL, 10);
+						i++;
+					}
+				}
+				i++;
+				break;
+					
+			case 'q':
+				flags &= ~(MSIEVE_FLAG_USE_LOGFILE |
+					   MSIEVE_FLAG_LOG_TO_STDOUT);
+				i++;
+				break;
+					
+			case 'd':
+				if (i + 1 < argc && isdigit(argv[i+1][0])) {
+					deadline = atol(argv[i+1]);
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+					
+			case 'r':
+				if (i + 1 < argc && isdigit(argv[i+1][0])) {
+					max_relations = atol(argv[i+1]);
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+					
+			case 't':
+				if (i + 1 < argc && isdigit(argv[i+1][0])) {
+					num_threads = atol(argv[i+1]);
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+
+			case 'D':
+				if (i + 1 < argc && isdigit(argv[i+1][0])) {
+					target_density = atof(argv[i+1]);
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+#ifdef HAVE_CUDA					
+			case 'g':
+				if (i + 1 < argc && isdigit(argv[i+1][0])) {
+					which_gpu = atol(argv[i+1]);
+					i += 2;
+				}
+				else {
+					print_usage(argv[0]);
+					return -1;
+				}
+				break;
+#endif					
+			case 'c':
+				flags |= MSIEVE_FLAG_SKIP_QS_CYCLES;
+				i++;
+				break;
+
+			case 'v':
+				flags |= MSIEVE_FLAG_LOG_TO_STDOUT;
+				i++;
+				break;
+
+			case 'p':
+				set_idle_priority();
+				i++;
+				break;
+
+			default:
+				print_usage(argv[0]);
+				return -1;
+			}
+		}
+		else {
+			if (isdigit(argv[i][0]) || argv[i][0] == '(' )
+				strncpy(buf, argv[i], sizeof(buf));
+			i++;
+		}
+	}
+
+	get_random_seeds(&seed1, &seed2);
+
+	if (deadline) {
+#if defined(WIN32) || defined(_WIN64)
+		DWORD thread_id;
+		CreateThread(NULL, 0, countdown_thread, 
+				&deadline, 0, &thread_id);
+#else
+		pthread_t thread_id;
+		pthread_create(&thread_id, NULL, 
+				countdown_thread, &deadline);
+#endif
+	}
+
+	if (isdigit(buf[0]) || buf[0] == '(' ) {
+		factor_integer(buf, flags, savefile_name, 
+				logfile_name, nfs_fbfile_name,
+				&seed1, &seed2,
+				max_relations, 
+				nfs_lower, nfs_upper, cpu,
+				cache_size1, cache_size2,
+				num_threads, mem_mb, 
+				which_gpu, target_density);
+	}
+	else if (manual_mode) {
+		while (1) {
+			printf("\n\nnext number: ");
+			fflush(stdout);
+			buf[0] = 0;
+			fgets(buf, (int)sizeof(buf), stdin);
+			factor_integer(buf, flags, savefile_name, 
+					logfile_name, nfs_fbfile_name,
+					&seed1, &seed2,
+					max_relations, 
+					nfs_lower, nfs_upper, cpu,
+					cache_size1, cache_size2,
+					num_threads, mem_mb, 
+					which_gpu, target_density);
+			if (feof(stdin))
+				break;
+		}
+	}
+	else {
+		FILE *infile = fopen(infile_name, "r");
+		if (infile == NULL) {
+			printf("cannot open input file '%s'\n", infile_name);
+			return 0;
+		}
+
+		while (1) {
+			buf[0] = 0;
+			fgets(buf, (int)sizeof(buf), infile);
+			factor_integer(buf, flags, savefile_name, 
+					logfile_name, nfs_fbfile_name,
+					&seed1, &seed2,
+					max_relations, 
+					nfs_lower, nfs_upper, cpu,
+					cache_size1, cache_size2,
+					num_threads, mem_mb, 
+					which_gpu, target_density);
+			if (feof(infile))
+				break;
+		}
+		fclose(infile);
+	}
+
+#ifdef HAVE_MPI
+	MPI_Finalize();
+#endif
+	return 0;
+}
---
+++ branches/cuda11_test/gnfs/poly/stage1/stage1.c
@@ -0,0 +1,394 @@
+/*--------------------------------------------------------------------
+This source distribution is placed in the public domain by its author,
+Jason Papadopoulos. You may use it for any purpose, free of charge,
+without having to notify anyone. I disclaim any responsibility for any
+errors.
+
+Optionally, please be nice and tell me if you find this source to be
+useful. Again optionally, if you add to the functionality present here
+please consider making those additions public too, so that others may 
+benefit from your work.	
+
+$Id$
+--------------------------------------------------------------------*/
+
+#include <stage1.h>
+
+/* main driver for stage 1 */
+
+/*------------------------------------------------------------------------*/
+static void
+poly_search_init(poly_search_t *poly, poly_stage1_t *data)
+{
+	mpz_init_set(poly->N, data->gmp_N);
+	mpz_init(poly->high_coeff);
+	mpz_init(poly->trans_N);
+	mpz_init(poly->trans_m0);
+	mpz_init(poly->mp_sieve_size);
+	mpz_init(poly->m);
+	mpz_init(poly->p);
+	mpz_init(poly->tmp1);
+	mpz_init(poly->tmp2);
+	mpz_init(poly->tmp3);
+	mpz_init(poly->tmp4);
+	mpz_init(poly->tmp5);
+
+	mpz_init_set(poly->gmp_high_coeff_begin, 
+			data->gmp_high_coeff_begin);
+	mpz_init_set(poly->gmp_high_coeff_end, 
+			data->gmp_high_coeff_end);
+
+	poly->degree = data->degree;
+	poly->norm_max = data->norm_max;
+	poly->callback = data->callback;
+	poly->callback_data = data->callback_data;
+
+#ifdef HAVE_CUDA
+	CUDA_TRY(cuCtxCreate(&poly->gpu_context, 
+			CU_CTX_BLOCKING_SYNC,
+			poly->gpu_info->device_handle))
+
+	/* load two GPU kernels, one for special-q
+	   collision search and one for ordinary collision 
+	   search */
+
+	CUDA_TRY(cuModuleLoad(&poly->gpu_module_sq, 
+				"stage1_core_gpu_sq.ptx"))
+	CUDA_TRY(cuModuleLoad(&poly->gpu_module_nosq, 
+				"stage1_core_gpu_nosq.ptx"))
+	CUDA_TRY(cuModuleLoad(&poly->gpu_module_sort, 
+				"stage1_core_gpu_sort.ptx"))
+#endif
+}
+
+/*------------------------------------------------------------------------*/
+static void
+poly_search_free(poly_search_t *poly)
+{
+	mpz_clear(poly->N);
+	mpz_clear(poly->high_coeff);
+	mpz_clear(poly->trans_N);
+	mpz_clear(poly->trans_m0);
+	mpz_clear(poly->mp_sieve_size);
+	mpz_clear(poly->m);
+	mpz_clear(poly->p);
+	mpz_clear(poly->tmp1);
+	mpz_clear(poly->tmp2);
+	mpz_clear(poly->tmp3);
+	mpz_clear(poly->tmp4);
+	mpz_clear(poly->tmp5);
+	mpz_clear(poly->gmp_high_coeff_begin);
+	mpz_clear(poly->gmp_high_coeff_end);
+#ifdef HAVE_CUDA
+	CUDA_TRY(cuCtxDestroy(poly->gpu_context)) 
+#endif
+}
+
+/*------------------------------------------------------------------------*/
+typedef struct {
+	uint32 p, r;
+	uint8 log_val;
+} sieve_prime_t;
+
+#define SIEVE_ARRAY_SIZE 8192
+
+typedef struct {
+	uint8 *sieve_array;
+	sieve_prime_t *primes;
+	uint32 num_primes;
+	uint32 num_primes_alloc;
+	uint32 curr_offset;
+} sieve_t;
+
+/*------------------------------------------------------------------------*/
+static void
+sieve_ad_block(sieve_t *sieve, poly_search_t *poly)
+{
+	uint32 i;
+	uint32 log_target;
+	double target;
+
+	target = mpz_get_d(poly->gmp_high_coeff_begin) /
+				HIGH_COEFF_MULTIPLIER;
+
+	if (HIGH_COEFF_SIEVE_LIMIT < target)
+		target = HIGH_COEFF_SIEVE_LIMIT;
+
+	log_target = floor(log(target) / M_LN2 + 0.5);
+	memset(sieve->sieve_array, (int)(log_target - 4),
+			SIEVE_ARRAY_SIZE);
+
+	for (i = 0; i < sieve->num_primes; i++) {
+		uint32 p = sieve->primes[i].p;
+		uint32 r = sieve->primes[i].r;
+		uint8 log_val = sieve->primes[i].log_val;
+
+		while (r < SIEVE_ARRAY_SIZE) {
+			sieve->sieve_array[r] -= log_val;
+			r += p;
+		}
+		sieve->primes[i].r = r - SIEVE_ARRAY_SIZE;
+	}
+}
+
+/*------------------------------------------------------------------------*/
+static int
+find_next_ad(sieve_t *sieve, poly_search_t *poly)
+{
+	uint32 i, j, p, k;
+	uint8 *sieve_array = sieve->sieve_array;
+
+	while (1) {
+
+		for (i = sieve->curr_offset; i < SIEVE_ARRAY_SIZE; i++) {
+
+			if (!(sieve_array[i] & 0x80))
+				continue;
+
+			mpz_divexact_ui(poly->tmp1, poly->gmp_high_coeff_begin,
+					(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+			mpz_add_ui(poly->tmp1, poly->tmp1, (mp_limb_t)i);
+			mpz_mul_ui(poly->high_coeff, poly->tmp1,
+					(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+
+			if (mpz_cmp(poly->high_coeff,
+						poly->gmp_high_coeff_end) > 0)
+				break;
+
+			/* trial divide the a_d and skip it if it
+			   does not have enough small factors */
+
+			mpz_cdiv_q_ui(poly->tmp2, poly->tmp1,
+				(mp_limb_t)HIGH_COEFF_SIEVE_LIMIT);
+			for (j = p = 0; j < PRECOMPUTED_NUM_PRIMES; j++) {
+				p += prime_delta[j];
+
+				if (p > HIGH_COEFF_PRIME_LIMIT)
+					break;
+
+				for (k = 0; k < HIGH_COEFF_POWER_LIMIT; k++) {
+					if (mpz_divisible_ui_p(poly->tmp1, 
+							(mp_limb_t)p))
+						mpz_divexact_ui(poly->tmp1, 
+							poly->tmp1,
+							(mp_limb_t)p);
+					else
+						break;
+				}
+			}
+			if (mpz_cmp(poly->tmp1, poly->tmp2) > 0)
+				continue;
+
+			/* a_d is okay, search it */
+
+			sieve->curr_offset = i + 1;
+			return 0;
+		}
+
+		/* update lower bound for next sieve block */
+
+		mpz_set_ui(poly->tmp1, (mp_limb_t)SIEVE_ARRAY_SIZE);
+		mpz_mul_ui(poly->tmp1, poly->tmp1,
+				(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+		mpz_add(poly->gmp_high_coeff_begin,
+				poly->gmp_high_coeff_begin, poly->tmp1);
+
+		if (mpz_cmp(poly->gmp_high_coeff_begin,
+					poly->gmp_high_coeff_end) > 0)
+			break;
+
+		sieve->curr_offset = 0;
+		sieve_ad_block(sieve, poly);
+	}
+
+	return 1;
+}
+
+/*------------------------------------------------------------------------*/
+static void
+init_ad_sieve(sieve_t *sieve, poly_search_t *poly)
+{
+	uint32 i, j, p;
+
+	sieve->num_primes = 0;
+	sieve->num_primes_alloc = 100;
+	sieve->primes = (sieve_prime_t *)xmalloc(sizeof(sieve_prime_t) *
+						sieve->num_primes_alloc);
+	sieve->sieve_array = (uint8 *)xmalloc(sizeof(uint8) *
+						SIEVE_ARRAY_SIZE);
+
+	mpz_divexact_ui(poly->tmp1, poly->gmp_high_coeff_begin,
+			(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+	for (i = p = 0; i < PRECOMPUTED_NUM_PRIMES; i++) {
+		uint32 power;
+		uint8 log_val;
+
+		p += prime_delta[i];
+		if (p > HIGH_COEFF_PRIME_LIMIT)
+			break;
+
+		log_val = floor(log(p) / M_LN2 + 0.5);
+		power = p;
+		for (j = 0; j < HIGH_COEFF_POWER_LIMIT; j++) {
+			uint32 r = mpz_cdiv_ui(poly->tmp1, (mp_limb_t)power);
+
+			if (sieve->num_primes >= sieve->num_primes_alloc) {
+				sieve->num_primes_alloc *= 2;
+				sieve->primes = (sieve_prime_t *)xrealloc(
+					sieve->primes,
+					sieve->num_primes_alloc *
+						sizeof(sieve_prime_t));
+			}
+
+			sieve->primes[sieve->num_primes].p = power;
+			sieve->primes[sieve->num_primes].r = r;
+			sieve->primes[sieve->num_primes].log_val = log_val;
+			sieve->num_primes++;
+
+			if ((uint32)(-1) / power < p)
+				break;
+
+			power *= p;
+		}
+	}
+
+	sieve->curr_offset = 0;
+	sieve_ad_block(sieve, poly);
+}
+
+/*------------------------------------------------------------------------*/
+static void
+free_ad_sieve(sieve_t *sieve)
+{
+	free(sieve->primes);
+	free(sieve->sieve_array);
+}
+
+/*------------------------------------------------------------------------*/
+static void
+search_coeffs(msieve_obj *obj, poly_search_t *poly, uint32 deadline)
+{
+	uint32 digits = mpz_sizeinbase(poly->N, 10);
+	double deadline_per_coeff;
+	double cumulative_time = 0;
+	sieve_t ad_sieve;
+
+	/* determine the CPU time limit; I have no idea if
+	   the following is appropriate */
+
+	if (digits <= 100)
+		deadline_per_coeff = 5;
+	else if (digits <= 105)
+		deadline_per_coeff = 20;
+	else if (digits <= 110)
+		deadline_per_coeff = 30;
+	else if (digits <= 120)
+		deadline_per_coeff = 50;
+	else if (digits <= 130)
+		deadline_per_coeff = 100;
+	else if (digits <= 140)
+		deadline_per_coeff = 200;
+	else if (digits <= 150)
+		deadline_per_coeff = 400;
+	else if (digits <= 175)
+		deadline_per_coeff = 800;
+	else if (digits <= 200)
+		deadline_per_coeff = 1600;
+	else
+		deadline_per_coeff = 3200;
+
+	printf("deadline: %.0lf CPU-seconds per coefficient\n",
+					deadline_per_coeff);
+
+	/* set up lower limit on a_d */
+
+	mpz_sub_ui(poly->tmp1, poly->gmp_high_coeff_begin, (mp_limb_t)1);
+	mpz_fdiv_q_ui(poly->tmp1, poly->tmp1, 
+			(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+	mpz_add_ui(poly->tmp1, poly->tmp1, (mp_limb_t)1);
+	mpz_mul_ui(poly->gmp_high_coeff_begin, poly->tmp1, 
+			(mp_limb_t)HIGH_COEFF_MULTIPLIER);
+
+	init_ad_sieve(&ad_sieve, poly);
+
+	while (1) {
+		double elapsed;
+
+		/* we only use a_d which are composed of
+		   many small prime factors, in order to
+		   have lots of projective roots going
+		   into stage 2 */
+
+		if (find_next_ad(&ad_sieve, poly))
+			break;
+
+		/* sieve for polynomials using
+		   Kleinjung's improved algorithm */
+
+		elapsed = sieve_lattice(obj, poly, deadline_per_coeff);
+		cumulative_time += elapsed;
+
+		if (obj->flags & MSIEVE_FLAG_STOP_SIEVING)
+			break;
+
+		if (deadline && cumulative_time > deadline)
+			break;
+	}
+
+	free_ad_sieve(&ad_sieve);
+}
+
+/*------------------------------------------------------------------------*/
+void
+poly_stage1_init(poly_stage1_t *data,
+		 stage1_callback_t callback, void *callback_data)
+{
+	memset(data, 0, sizeof(poly_stage1_t));
+	mpz_init_set_ui(data->gmp_N, (mp_limb_t)0);
+	mpz_init_set_ui(data->gmp_high_coeff_begin, (mp_limb_t)0);
+	mpz_init_set_ui(data->gmp_high_coeff_end, (mp_limb_t)0);
+	data->callback = callback;
+	data->callback_data = callback_data;
+}
+
+/*------------------------------------------------------------------------*/
+void
+poly_stage1_free(poly_stage1_t *data)
+{
+	mpz_clear(data->gmp_N);
+	mpz_clear(data->gmp_high_coeff_begin);
+	mpz_clear(data->gmp_high_coeff_end);
+}
+
+/*------------------------------------------------------------------------*/
+void
+poly_stage1_run(msieve_obj *obj, poly_stage1_t *data)
+{
+	/* pass external configuration in and run the search */
+
+	poly_search_t poly;
+#ifdef HAVE_CUDA
+	gpu_config_t gpu_config;
+
+	gpu_init(&gpu_config);
+	if (gpu_config.num_gpu == 0) {
+		printf("error: no CUDA-enabled GPUs found\n");
+		exit(-1);
+	}
+	if (obj->which_gpu >= (uint32)gpu_config.num_gpu) {
+		printf("error: GPU %u does not exist "
+			"or is not CUDA-enabled\n", obj->which_gpu);
+		exit(-1);
+	}
+	logprintf(obj, "using GPU %u (%s)\n", obj->which_gpu,
+			gpu_config.info[obj->which_gpu].name);
+
+	poly.gpu_info = gpu_config.info + obj->which_gpu; 
+#endif
+
+	poly_search_init(&poly, data);
+
+	search_coeffs(obj, &poly, data->deadline);
+
+	poly_search_free(&poly);
+}
