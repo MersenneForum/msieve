cc09f88a419a00c0a8bd643c161ce39ceef30ab4
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2012-08-12 12:29:34 +0000

    initial commit of Sean Baxter's GPU sorting library

---
+++ kernels/countgen.cu
@@ -0,0 +1,84 @@
+#include "params.cu"
+#define NUM_WARPS NUM_COUNT_WARPS
+
+#include "countcommon.cu"
+
+#define NUM_THREADS (NUM_WARPS * WARP_SIZE)
+#define GATHER_SUM_MODE 1		// change to 2 for larger blocks.
+
+
+__device__ uint sortDetectCounters_global[4];
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Count kernels without early exit detection
+
+#define NUM_BITS 1
+#define COUNT_FUNC CountBuckets_1
+#define COUNT_SHARED_MEM count_shared1
+#include "count.cu"
+
+#define NUM_BITS 2
+#define COUNT_FUNC CountBuckets_2
+#define COUNT_SHARED_MEM count_shared2
+#include "count.cu"
+
+#define NUM_BITS 3
+#define COUNT_FUNC CountBuckets_3
+#define COUNT_SHARED_MEM count_shared3
+#include "count.cu"
+
+#define NUM_BITS 4
+#define COUNT_FUNC CountBuckets_4
+#define COUNT_SHARED_MEM count_shared4
+#include "count.cu"
+
+#define NUM_BITS 5
+#define COUNT_FUNC CountBuckets_5
+#define COUNT_SHARED_MEM count_shared5
+#include "count.cu"
+
+#define NUM_BITS 6
+#define COUNT_FUNC CountBuckets_6
+#define COUNT_SHARED_MEM count_shared6
+#include "count.cu"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Count kernels with early exit detection
+
+#if __CUDA_ARCH__ >= 200
+// for non-Fermi cards, the early-exit kernels are no-ops
+// (no ballot instruction)
+#define DETECT_SORTED
+#endif
+
+#define NUM_BITS 1
+#define COUNT_FUNC CountBuckets_ee_1
+#define COUNT_SHARED_MEM count_shared_ee1
+#include "count.cu"
+
+#define NUM_BITS 2
+#define COUNT_FUNC CountBuckets_ee_2
+#define COUNT_SHARED_MEM count_shared_ee2
+#include "count.cu"
+
+#define NUM_BITS 3
+#define COUNT_FUNC CountBuckets_ee_3
+#define COUNT_SHARED_MEM count_shared_ee3
+#include "count.cu"
+
+#define NUM_BITS 4
+#define COUNT_FUNC CountBuckets_ee_4
+#define COUNT_SHARED_MEM count_shared_ee4
+#include "count.cu"
+
+#define NUM_BITS 5
+#define COUNT_FUNC CountBuckets_ee_5
+#define COUNT_SHARED_MEM count_shared_ee5
+#include "count.cu"
+
+#define NUM_BITS 6
+#define COUNT_FUNC CountBuckets_ee_6
+#define COUNT_SHARED_MEM count_shared_ee6
+#include "count.cu"
---
+++ kernels/params.cu
@@ -0,0 +1,8 @@
+#pragma once
+
+
+// Project-wide constants for the ModernGPU sort library.
+
+#define NUM_COUNT_WARPS 4
+
+#define NUM_HIST_WARPS 32
