4b3c742805f0abb45d3f1025de0220fd29e80278
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2012-11-03 15:35:48 +0000

    reduce the maximum number of pieces that large search ranges are divided into

--- gnfs/poly/stage1/stage1_sieve_gpu.c
+++ gnfs/poly/stage1/stage1_sieve_gpu.c
@@ -955,9 +955,11 @@ sieve_lattice_gpu_core(msieve_obj *obj,
 	   multiple runs over the same range of leading
 	   a_d will likely generate different results */
 
-	num_pieces = MIN(450, (double)special_q_max * p_max
+	num_pieces = 1;
+	if (special_q_max - special_q_min > 500000)
+		num_pieces = MIN(50, (double)special_q_max * p_max
 				/ log(special_q_max) / log(p_max)
-				/ 3e9);
+				/ 3e10);
 
 	if (num_pieces > 1) { /* randomize the special_q range */
 		uint32 piece_length = (special_q_max - special_q_min)
