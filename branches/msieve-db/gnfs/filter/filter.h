6fc512a7c2a1276baf16a17ec7c000fc22a53339
jasonp_sf <jasonp_sf@8c02911e-0617-4013-80d0-c3bcbcae04d5>
2013-03-09 14:52:14 +0000

    add experimental code that reads relations and writes to a Berkeley database

--- branches/msieve-db/gnfs/filter/filter.h
+++ branches/msieve-db/gnfs/filter/filter.h
@@ -31,8 +31,8 @@ extern "C" {
    value is the large prime bound to use for the singleton removal */
 
 uint32 nfs_purge_duplicates(msieve_obj *obj, factor_base_t *fb,
-				uint32 max_relations,
-				uint32 *num_relations_out); 
+				uint64 max_relations,
+				uint64 *num_relations_out); 
 
 /* read '<savefile_name>.d' and create '<savefile_name>.lp', a 
    binary file containing the relations surviving the singleton
